a8eafa89a85da13e99548d6090c9b6b853dc3710
Isaac Tan <iztan98@gmail.com>
2024-02-23 17:56:01 +0800

    add: tmdb

--- src/routes/meta/index.ts
+++ src/routes/meta/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import anilist from "./anilist";
+import tmdb from "./tmdb";
 
 
 const router = express.Router();
 
 router.use("/anilist", anilist)
+router.use("/tmdb", tmdb)
 
 export default router;
---
+++ src/routes/meta/tmdb.ts
@@ -0,0 +1,65 @@
+import express, { Request, Response } from "express";
+import { ANIME, META, PROVIDERS_LIST } from "@consumet/extensions";
+
+const router = express.Router();
+
+const tmdbApi = process.env.TMDB_API_KEY;
+
+router.get('/search/:query', async (request: Request, reply: Response) => {
+    const query = (request.params as { query: string }).query;
+    const page = (request.query as unknown as { page: number }).page;
+    const tmdb = new META.TMDB(tmdbApi);
+
+    const res = await tmdb.search(query, page);
+
+    reply.status(200).send(res);
+});
+
+router.get('/info/:id', async (request: Request, reply: Response) => {
+    const id = (request.params as { id: string }).id;
+    const type = (request.query as { type: string }).type;
+    const provider = (request.query as { provider?: string }).provider;
+    let tmdb = new META.TMDB(tmdbApi);
+
+    if (!type) return reply.status(400).send({ message: "The 'type' query is required" });
+
+    if (typeof provider !== 'undefined') {
+        const possibleProvider = PROVIDERS_LIST.MOVIES.find(
+            (p) => p.name.toLowerCase() === provider.toLocaleLowerCase(),
+        );
+        tmdb = new META.TMDB(tmdbApi, possibleProvider);
+    }
+
+    const res = await tmdb.fetchMediaInfo(id, type);
+    reply.status(200).send(res);
+});
+
+router.get(
+    '/watch/:episodeId',
+    async (request: Request, reply: Response) => {
+        const episodeId = (request.params as { episodeId: string }).episodeId;
+        const id = (request.query as { id: string }).id;
+        const provider = (request.query as { provider?: string }).provider;
+
+        let tmdb = new META.TMDB(tmdbApi);
+        if (typeof provider !== 'undefined') {
+            const possibleProvider = PROVIDERS_LIST.MOVIES.find(
+                (p) => p.name.toLowerCase() === provider.toLocaleLowerCase(),
+            );
+            tmdb = new META.TMDB(tmdbApi, possibleProvider);
+        }
+        try {
+            const res = await tmdb
+                .fetchEpisodeSources(episodeId, id)
+                .catch((err) => reply.status(404).send({ message: err }));
+
+            reply.status(200).send(res);
+        } catch (err) {
+            reply
+                .status(500)
+                .send({ message: 'Something went wrong. Contact developer for help.' });
+        }
+    },
+);
+
+export default router;
